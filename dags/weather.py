a1d2718f01b1cb55cc0e42ced70124d04db3d085
safem0de <44099877+safem0de@users.noreply.github.com>
2025-02-08 08:38:14 +0000

    2025-02-08 update dag validate, mail hog, adminer

--- dags/weather.py
+++ dags/weather.py
@@ -1,12 +1,22 @@
+import json
+import requests
+from datetime import datetime, timedelta
+
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.email import EmailOperator
 from airflow.operators.python import PythonOperator
 from airflow.utils import timezone
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-import requests
+
+DAG_FILE_NAME = "/opt/airflow/dags/data.json"
+CONN_STR = "weather_postgres_conn"
 
 
 def _get_weather_data():
+    # assert 1 == 2
+
     API_KEY = "7958cef1ef4a1596e89c6e60a3cdfcc3"
     # API_KEY = os.environ.get("WEATHER_API_KEY")
     # API_KEY = Variable.get("weather_api_key")
@@ -23,11 +33,65 @@ def _get_weather_data():
     data = response.json()
     print(data)
 
+    with open(DAG_FILE_NAME, "w") as f:
+        json.dump(data, f)
 
+
+def _create_weather_table():
+    pg_hook = PostgresHook(
+        postgres_conn_id=CONN_STR,
+        schema="postgres"
+    )
+    connection = pg_hook.get_conn()
+    cursor = connection.cursor()
+
+    sql = """
+        CREATE TABLE IF NOT EXISTS weathers (
+            dt BIGINT NOT NULL,
+            temp FLOAT NOT NULL
+        )
+    """
+    cursor.execute(sql)
+    connection.commit()
+
+
+def _load_data_to_postgres():
+    pg_hook = PostgresHook(
+        postgres_conn_id=CONN_STR,
+        schema="postgres"
+    )
+    connection = pg_hook.get_conn()
+    cursor = connection.cursor()
+
+    with open(DAG_FILE_NAME, "r") as f:
+        data = json.load(f)
+
+    temp = data["main"]["temp"]
+    dt = data["dt"]
+    sql = f"""
+        INSERT INTO weathers (dt, temp) VALUES ({dt}, {temp})
+    """
+    cursor.execute(sql)
+    connection.commit()
+
+
+def _validate_data():
+    with open(DAG_FILE_NAME, "r") as f:
+        data = json.load(f)
+
+    assert data.get("main") is not None
+
+
+default_args = {
+    "email": ["67130503@dpu.ac.th"],
+    "retries": 3,
+    "relay_delay": timedelta(minutes=1)
+}
 with DAG(
     "weather_api_dag",
-    schedule="@hourly",
-    start_date=timezone.datetime(2025, 1, 31),
+    default_args=default_args,
+    schedule="0 */3 * * *",
+    start_date=timezone.datetime(2025, 2, 1),
     tags=["dpu"]
 ):
     start = EmptyOperator(task_id="start")
@@ -37,6 +101,30 @@ with DAG(
         python_callable=_get_weather_data,
     )
 
+    create_weather_table = PythonOperator(
+        task_id="create_weather_table",
+        python_callable=_create_weather_table,
+    )
+
+    load_data_to_postgres = PythonOperator(
+        task_id="load_data_to_postgres",
+        python_callable=_load_data_to_postgres,
+    )
+
+    validate_data = PythonOperator(
+        task_id="validate_data",
+        python_callable=_validate_data,
+    )
+
+    send_email = EmailOperator(
+        task_id="send_email",
+        to=["kan@odds.team"],
+        subject="Finished getting open weather data",
+        html_content="Done",
+    )
+
     end = EmptyOperator(task_id="end")
 
-    start >> get_weather_data >> end
+
+    start >> get_weather_data >> validate_data >> load_data_to_postgres >> send_email >> end
+    start >> create_weather_table >> load_data_to_postgres >> send_email >>end
